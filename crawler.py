5c49464f12d9362bd49e39ee6e77c5a566765f40
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2025-06-20 14:10:38 +0100

    creates run function for main programme to run and creates function to count the response time

--- crawler.py
+++ crawler.py
@@ -1,9 +1,9 @@
-import requests
+import requests, time
 from bs4 import BeautifulSoup
 
 
 class WebPageReport():
-    """This is the class that defines the crawler, it's functions and attributes"""
+    """This is the class that defines the crawler, it's functions and attributes."""
     def __init__(self, url):
         """This initialises the crawler and sets out the the metrics gathered by the page report."""
         
@@ -27,6 +27,18 @@ class WebPageReport():
         self.broken_links = []
         self.internal_links_count = 0
         self.external_links_count = 0
+    
+    def run(self):
+        
+        self.response_time = self.count_response_time()
+
+    def count_response_time(self):
+        """Counts the time a http request to the page takes to complete."""
+        start_time = time.time()
+        response = requests.get(self.url)
+        end_time = time.time()
+        response_time = end_time - start_time
+        return response_time
 
 
 quality_checker = WebPageReport("https://developer.mozilla.org")
